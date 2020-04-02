c1f265feb28eb28d031f54af101788aeaa5f23fc
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2020-04-01 23:38:06 -0400

    Add beginning of soil temp test sketch

---
+++ sensor_tests/soil_temp_test/soil_temp_test.ino
@@ -0,0 +1,24 @@
+/*
+ * Author:  Daniel Mallia
+ * Date:    4/1/2020
+ * Purpose: This script is used for testing the Gikfun waterproof
+ *          DS18B20 temperature probe sensor. 
+ *          
+ * Refer to:         
+ * https://github.com/adafruit/MAX31850_DallasTemp/blob/master/examples/Single/Single.pde
+ * https://www.hacktronics.com/Tutorials/arduino-1-wire-tutorial.html
+ * 
+ */
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+
+}
