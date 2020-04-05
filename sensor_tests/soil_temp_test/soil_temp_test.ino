bd4d3a462245d69ea1e0822d4d1e5dd19558948f
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2020-04-04 22:26:10 -0400

    Expand header documentation

--- sensor_tests/soil_temp_test/soil_temp_test.ino
+++ sensor_tests/soil_temp_test/soil_temp_test.ino
@@ -4,18 +4,39 @@
  * Purpose: This script is used for testing the Gikfun waterproof
  *          DS18B20 temperature probe sensor. A saint of a 
  *          reviewer on Amazon described the proper wiring and 
- *          referred to the required libaries; another commenter
- *          suggested the hackatronics tutorial. Please note that
- *          in the course of looking up the needed OneWire and 
- *          DallasTempetature libraries, I found that Adafruit 
- *          had expanded on the original libraries, and so I 
- *          opted to use their versions instead. 
- *           
- *           
+ *          referred to the required libaries; a commenter on the
+ *          review suggested the hackatronics tutorial. (See the 
+ *          below links.)
  *          
+ *          Please note that in the course of looking up the 
+ *          needed OneWire and DallasTempetature libraries, I 
+ *          found that Adafruit had expanded on the original 
+ *          libraries, and so I opted to use their versions 
+ *          instead. Thus the below uses the following Adafruit 
+ *          libraries:
+ *          
+ *          Adafruit MAX31850 DallasTemp Version 1.0.1
+ *          Adafruit MAX31850 OneWire Version 1.0.2
+ *          
+ *          Both were installed via the Arduino IDE library 
+ *          manager. 
+ *           
+ *          Two sensor notes:
+ *          1. This particular DS18B20 sensor does not use 
+ *             "parasitic power" as it has a dedicated VCC 
+ *             (Voltage Common Collector) line. This   
+ *             behavior can be checked via the 
+ *             DallasTemperature isParasitePowerMode() 
+ *             method.
+ *          2. Multiple DS18B20 sensors can be connected to
+ *             the same data pin: hence the need to find the
+ *             individual device addresses.
+ *             
  * Refer to:         
- * https://github.com/adafruit/MAX31850_DallasTemp/blob/master/examples/Single/Single.pde
+ * https://www.amazon.com/gp/customer-reviews/R3MCZMSAU1CUER/ref=cm_cr_dp_d_rvw_ttl?ie=UTF8&ASIN=B012C597T0
  * https://www.hacktronics.com/Tutorials/arduino-1-wire-tutorial.html
+ * https://github.com/adafruit/MAX31850_DallasTemp/blob/master/examples/Single/Single.pde
+ * https://github.com/adafruit/MAX31850_DallasTemp/blob/master/DallasTemperature.h
  * 
  */
 
