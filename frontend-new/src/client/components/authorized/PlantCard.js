b80c8b193526cdbb55b3ad5ec5923caa53035f59
Stanley <lims95@yahoo.com>
2020-04-05 21:36:47 -0400

    added modal, images, improved UI/UX

--- frontend-new/src/client/components/authorized/PlantCard.js
+++ frontend-new/src/client/components/authorized/PlantCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Popup from "reactjs-popup";
+import PlantInfo from './PlantInfo';
 
 const PlantCard = props => {
     const { plant } = props;
     return (
         <div id='plant-card'>
-            <Link to={`/user/plants/${plant.plant_id}`} id={`${plant.name}-image`}>{plant.name}</Link>
+            <Popup modal trigger={
+                <h1 id={`${plant.species}-image`}>{`${plant.plant_name[0].toUpperCase()}${plant.plant_name.substring(1, plant.plant_name.length)}`}</h1>
+            }>
+                <PlantInfo plant={ plant } />
+            </Popup>
+
+
+            <div id='plant-info'>
+                <h1>Plant Information</h1>
+                <h2>Species: { plant.species }</h2>
+                <h2>User email: { plant.user_email }</h2>
+                <h2>Date created: { plant.date_created } </h2>
+            </div>
         </div>
     );  
 };
--- frontend-new/src/server/api.js
+++ frontend-new/src/server/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const API = 'http://127.0.0.1:5000/plant';
+const API = 'http://127.0.0.1:5000/plants/';
 
 router.get('/plants', async (req, res, next) => {
     try {
@@ -15,7 +15,7 @@ router.get('/plants', async (req, res, next) => {
 
 router.get('/plants/:id', async (req, res, next) => {
     try {
-        const data = (await axios.get(`${API}/${req.params.id}`)).data;
+        const data = (await axios.get(`${API}/plant/${req.params.id}`)).data;
         res.status(201).json(data);
     } catch(err) {
         console.log(err);
