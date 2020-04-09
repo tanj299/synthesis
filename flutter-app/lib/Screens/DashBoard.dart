867e9deb486ad50ea7861ecbf773993cd341731d
LeoAuyeung <leo297747@gmail.com>
2020-04-09 05:42:37 -0400

    Minor text fixes

--- flutter-app/lib/Screens/DashBoard.dart
+++ flutter-app/lib/Screens/DashBoard.dart
@@ -52,81 +52,6 @@ class _DashboardState extends State<Dashboard> {
                     ],
                   ),
                 ),
-                // Padding(
-                //   padding: const EdgeInsets.only(top: 20),
-                //   child: Row(
-                //     crossAxisAlignment: CrossAxisAlignment.center,
-                //     children: <Widget>[
-                //       BoldText("Recommended for you", 20.0, kblack),
-                //       SizedBox(
-                //         width: 60,
-                //       ),
-                //       BoldText("More", 15.0, korange),
-                //       Icon(
-                //         Icons.navigate_next,
-                //         color: korange,
-                //       )
-                //     ],
-                //   ),
-                // ),
-                // Container(
-                //   width: 400,
-                //   height: 200,
-                //   child: ListView(
-                //     scrollDirection: Axis.horizontal,
-                //     children: <Widget>[
-                //       RecommendationImage("assets/sheraton.jpg", "Sheraton", "Oran"),
-                //       RecommendationImage("assets/Meridien.jpg", "Meridien", "Oran"),
-                //       RecommendationImage("assets/ibis.jpg", "Ibis", "Oran"),
-                //     ],
-                //   ),
-                // ),
-                // Padding(
-                //   padding: const EdgeInsets.only( top: 10,bottom: 16.0),
-                //   child: Align(
-                //       alignment: Alignment.centerLeft,
-                //       child: BoldText("Best Rated Places", 20.0, kblack)),
-                // ),
-                //  Container(
-                //     width: 400,
-                //     height: 250,
-                //     child: ListView(
-                //       scrollDirection: Axis.horizontal,
-                //       children: <Widget>[
-                //         BestRatedImage(
-                //             "assets/sheraton.jpg", "Sheraton", "Oran", 4.5),
-                //         BestRatedImage(
-                //             "assets/Meridien.jpg", "Meridien", "Oran", 4.8),
-                //         BestRatedImage("assets/ibis.jpg", "Ibis", "Oran", 3.1),
-                //       ],
-
-                //   ),
-                // ),
-                // Padding(
-                //   padding: const EdgeInsets.only(top: 10,bottom: 16.0),
-                //   child: Align(
-                //       alignment: Alignment.centerLeft,
-                //       child: BoldText("Awesome Cities", 20.0, kblack)),
-                // ),
-                // Row(
-                //   children: <Widget>[
-                //     CitiesImage("assets/alger.jpg","ALGIERS"),
-                //     SizedBox(width: 28,),
-                //     CitiesImage("assets/tlemcen.jpg","TLEMCEN"),
-
-                //   ],
-                // ),
-                // SizedBox(height: 28,),
-                //   Row(
-                //     children: <Widget>[
-                //       CitiesImage("assets/adrar.jpg","ADRAR"),
-                //       SizedBox(width: 28,),
-
-                //       CitiesImage("assets/bedjaia.jpg","BEDJAIA"),
-
-                //     ],
-
-                // ),
 
               ]),
             ),
@@ -164,10 +89,6 @@ class _DashboardState extends State<Dashboard> {
                       borderRadius: new BorderRadius.only(
                           topLeft: Radius.circular(15),
                           bottomLeft: Radius.circular(15)),
-                      // child: Image.asset(
-                      //   "assets/hotel.jpg",
-                      //   fit: BoxFit.fitHeight,
-                      // )
                       child: Icon(
                           Icons.local_florist,
                           color: kgreyDark,
@@ -231,36 +152,11 @@ class _DashboardState extends State<Dashboard> {
                       height: 30,
                     ),
                     SizedBox(height: 14),
-                    // Row(
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //   children: <Widget>[
-                    //     SizedBox(
-                    //       width: 90,
-                    //     ),
-                    //     BoldText("More", 12.0, kblack),
-                    //     Icon(
-                    //       Icons.navigate_next,
-                    //       size: 15.0,
-                    //     ),
-                    //   ],
-                    // )
                   ],
                 )
               ],
             )),
       ),
-    );
-  }
-
-  Row imagesRow() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        SquaredIcon(Icons.airplanemode_active, "Flights"),
-        SquaredIcon(FontAwesomeIcons.hotel, "Hotels"),
-        SquaredIcon(Icons.directions_car, "Cars"),
-      ],
     );
   }
 }
