08e359ff0e0e83dbcc4f11e11eae69b5dd6bc36d
tanj299 <tanj299@gmail.com>
2020-05-04 17:47:15 -0400

    modified main.py to application.py

--- backend/application.py
+++ backend/application.py
@@ -5,7 +5,7 @@
 # Date Begun: 03/25/2020
 # Last Updated: 04/25/2020
 # Driver application of the REST API
-# Note: Consider looking into dotenv to set flask app variables without having to set it in the terminal every time
+# Note: Consider looking into dotenv to set flask application variables without having to set it in the terminal every time
 # DL: Delete later, used for testing purposes
 # https://github.com/maxcountryman/flask-login
 # https://codereview.stackexchange.com/questions/110679/simple-login-system-using-python-flask-and-mysql
@@ -26,29 +26,29 @@ from logs import logs_api
 
 # Flask application
 # DL: secret key
-app = Flask(__name__)
-app.secret_key = 'secret_test'
+application = Flask(__name__)
+application.secret_key = 'secret_test'
 
 # mysql config
-app.config['SECRET_KEY'] = 'synthesizeMeCaptain'
-app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'synthesize0220'
-app.config['MYSQL_DATABASE_DB'] = 'synthesis'
-app.config['MYSQL_DATABASE_HOST'] = 'synthesis-database.c0a8ellxvhhd.us-east-1.rds.amazonaws.com'
-mysql.init_app(app)
+application.config['SECRET_KEY'] = 'synthesizeMeCaptain'
+application.config['MYSQL_DATABASE_USER'] = 'admin'
+application.config['MYSQL_DATABASE_PASSWORD'] = 'synthesize0220'
+application.config['MYSQL_DATABASE_DB'] = 'synthesis'
+application.config['MYSQL_DATABASE_HOST'] = 'synthesis-database.c0a8ellxvhhd.us-east-1.rds.amazonaws.com'
+mysql.init_app(application)
 
 # Register Blueprint objects with the application to modularize our code 
-app.register_blueprint(plants_api, url_prefix = "/plants")
-app.register_blueprint(config_api, url_prefix = "/config")
-app.register_blueprint(login_api, url_prefix = "/login")
-app.register_blueprint(requests_api, url_prefix = "/requests")
-app.register_blueprint(logs_api, url_prefix = "/logs")
+application.register_blueprint(plants_api, url_prefix = "/plants")
+application.register_blueprint(config_api, url_prefix = "/config")
+application.register_blueprint(login_api, url_prefix = "/login")
+application.register_blueprint(requests_api, url_prefix = "/requests")
+application.register_blueprint(logs_api, url_prefix = "/logs")
 
-# Initialize Flask-Login immediately after app
-login = LoginManager(app)
+# Initialize Flask-Login immediately after application
+login = LoginManager(application)
 
 # Index route; test backend
-@app.route('/')
+@application.route('/')
 def index():
     return('Welcome to the backend!')
 
@@ -56,4 +56,4 @@ def index():
 # If main.py is imported to another file, then it is NOT "__main__" 
 # __name__ will be set to the file that the other file is named
 if __name__ == "__main__":
-    app.run()
+    application.run(host='0.0.0.0')
